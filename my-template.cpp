142776853c68a7dd5559cff5d334556fb8678692
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-02-16 10:34:50 +0700

    Update my-template.cpp

--- my-template.cpp
+++ my-template.cpp
@@ -1,25 +1,31 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-#define fl float
-#define el "\n"
 #define ull unsigned long long
+#define db double
+#define fl float
 
-const int maxn=10e6+42;
-int a[maxn];
+#define el "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define mp ios::sync_with_stdio(false);cin.tie(nullptr);
 
+const int maxn=1e6+42;
+const int N=1e3;
+ll a[maxn], y[N][N], dem[maxn];
 
 void solve(){
     
-    
 
+    
 }
 
 int main(){
-    //freopen(".INP", "r", stdin);
-    //freopen(".OUT", "w", stdout);
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    //freopen("DAUNGOAC.INP","r",stdin);
+    //freopen("DAUNGOAC.OUT","w",stdout);
+    mp
     solve();
 }
