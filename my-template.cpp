cb0e2361a15a2b1571798cf2be1e05d419b0a60d
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-05-12 23:56:28 +0700

    Refactor and optimize my-template.cpp

--- my-template.cpp
+++ my-template.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -6,40 +8,62 @@ using namespace std;
 #define ull unsigned long long
 #define db double
 #define fl float
-
-#define el "\n"
+#define str string
+#define ldb long double
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define bend(x) (x).begin(), (x).end()
+#define ret return
+#define el '\n'
+#define si size()
 #define fi first
 #define se second
 #define pb push_back
+#define pob pop_back
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
 
-void mp(){
+const ll INF = 1e18, NINF = -1e18;
+const int MAXN=1e6+42, N=1e3, MOD=1000000007;
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[N][N], pre[MAXN];
+
+void manhphong(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
 
 void ffopen(string name){
-    string inFile=name + ".inp";
-    string outFile=name + ".out";
-    if(FILE* f = fopen(inFile.c_str(), "r")){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
         fclose(f);
-        freopen(inFile.c_str(), "r", stdin);
-        freopen(outFile.c_str(), "w", stdout);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
     }
 }
-
-const int maxn=1e6+42;
-const int N=1e3;
-ll a[maxn], y[N][N], dem[maxn];
 
 void solve(){
     
 
-    
+
 }
 
 int main(){
     //ffopen("filename");
-    mp();
+    manhphong();
     solve();
 }
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
